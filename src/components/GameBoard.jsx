1305a9b79c3f6ab0faffba9c2ccdf8310b7cf678
Asif Ali <asifalieas@gmail.com>
2024-12-28 16:04:29 +0530

    Lift the state up : switching turn and Symbol between players in UI

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,7 +6,7 @@ const initialGameBoard = [
   [null, null, null],
 ]
 
-export default function GameBoard(rowIndex, colIndex) {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard)
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -14,9 +14,11 @@ export default function GameBoard(rowIndex, colIndex) {
       const updatedGameBoard = [
         ...previousGameBoard.map((innerArray) => [...innerArray]),
       ]
-      updatedGameBoard[rowIndex][colIndex] = 'X'
+      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol
       return updatedGameBoard
     })
+
+    onSelectSquare()
   }
 
   return (
